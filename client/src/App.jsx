17c62d1361602f01c7b03d49454181fdb5107e46
AriKodcode <arikodcode@gmail.com>
2026-03-17 12:59:32 +0200

    create login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,11 +4,13 @@ import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import LauncherDetalis from "./pages/LauncherDetalis";
 import AddLauncherPage from "./pages/AddLauncherPage";
+import Login from "./components/Login";
 export default function App() {
   return (
     <div className="app">
       <Header />
       <Routes>
+        <Login path="/" element={<Login />} />
         <Route path="/launchers" element={<HomePage />} />
         <Route path="/details/:id" element={<LauncherDetalis />} />
         <Route path="/added/launcher" element={<AddLauncherPage />} />
